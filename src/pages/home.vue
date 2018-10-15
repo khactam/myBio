dd9d63f334419e96dc4d6849202ef6c97ab8d581
khactam <khactam95@gmail.com>
2018-10-15 16:16:30 +0300

    inject skills component

--- src/pages/home.vue
+++ src/pages/home.vue
@@ -1,27 +1,21 @@
 <template>
   <f7-page>
+    <!-- header -->
     <f7-navbar>
       <f7-nav-left>
         <f7-link icon-if-ios="f7:menu" icon-if-md="material:menu" panel-open="left"></f7-link>
       </f7-nav-left>
       <f7-nav-title>My personal app</f7-nav-title>
-      <f7-nav-right>
-        <f7-link icon-if-ios="f7:menu" icon-if-md="material:menu" panel-open="right"></f7-link>
-      </f7-nav-right>
     </f7-navbar>
-    <f7-toolbar>
-      <f7-link>Left Link</f7-link>
-      <f7-link>Right Link</f7-link>
-    </f7-toolbar>
-    <f7-block strong>
-      <p>Here is your blank Framework7 app. Let's see what we have here.</p>
-    </f7-block>
+    <!-- main body -->
     <f7-block-title>Navigation</f7-block-title>
     <f7-list>
       <f7-list-item link="/about/" title="About"></f7-list-item>
       <f7-list-item link="/form/" title="Form"></f7-list-item>
     </f7-list>
-    <f7-block-title>Modals</f7-block-title>
+    <skills></skills>
+
+    <!-- <f7-block-title>Modals</f7-block-title>
     <f7-block strong>
       <f7-row>
         <f7-col width="50">
@@ -31,24 +25,19 @@
           <f7-button fill raised login-screen-open="#login-screen">Login Screen</f7-button>
         </f7-col>
       </f7-row>
-    </f7-block>
-    <f7-block-title>Panels</f7-block-title>
-    <f7-block strong>
-      <f7-row>
-        <f7-col width="50">
-          <f7-button fill raised panel-open="left">Left Panel</f7-button>
-        </f7-col>
-        <f7-col width="50">
-          <f7-button fill raised panel-open="right">Right Panel</f7-button>
-        </f7-col>
-      </f7-row>
-    </f7-block>
-    <f7-list>
-      <f7-list-item link="/dynamic-route/blog/45/post/125/?foo=bar#about" title="Dynamic Route"></f7-list-item>
-      <f7-list-item link="/load-something-that-doesnt-exist/" title="Default Route (404)"></f7-list-item>
-    </f7-list>
+    </f7-block> -->
+    <!-- bottom -->
+    <f7-toolbar :bottom-md="true">
+      <f7-link>Left Link</f7-link>
+      <f7-link>Right Link</f7-link>
+    </f7-toolbar>
   </f7-page>
 </template>
 <script>
-export default {}
+import Skills from './skills.vue'
+export default {
+  components: {
+    skills: Skills
+  }
+}
 </script>
