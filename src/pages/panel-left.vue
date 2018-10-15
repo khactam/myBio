a9cf5b1ca57669eeb828204ac7e396b2d36118f9
khactam <khactam95@gmail.com>
2018-10-15 14:11:38 +0300

    minor changes to personalize

--- src/pages/panel-left.vue
+++ src/pages/panel-left.vue
@@ -1,21 +1,22 @@
 <template>
   <f7-page>
-    <f7-navbar title="Left Panel"></f7-navbar>
-    <f7-block strong>
-      <p>Left panel content goes here</p>
-    </f7-block>
-    <f7-block-title>Load page in panel</f7-block-title>
-    <f7-list>
-      <f7-list-item link="/about/" title="About"></f7-list-item>
-      <f7-list-item link="/form/" title="Form"></f7-list-item>
-    </f7-list>
-    <f7-block-title>Load page in main view</f7-block-title>
-    <f7-list>
-      <f7-list-item link="/about/" title="About" view="#main-view" panel-close></f7-list-item>
-      <f7-list-item link="/form/" title="Form" view="#main-view" panel-close></f7-list-item>
+    <f7-navbar title="BIO"></f7-navbar>
+    <f7-card
+  content="Hello, few words about me. I'm a developer who love doing right thing but still drink when I code so thing is not so right. Love being somewhere that is strange and new (can be a new house of a lady) or just being somewhere peaceful (like in random lady's bed)."></f7-card>
+    <f7-list media-list inset>
+      <f7-list-item
+        link="https://www.youtube.com/watch?v=PRXtbLqIx04"
+        title="My anthem"
+        subtitle="Peach Pit">
+        <img slot="media" src="../../static/img/peachpit.jpg" width="50" />
+      </f7-list-item>
     </f7-list>
   </f7-page>
 </template>
 <script>
 export default {}
 </script>
+<style scoped>
+
+</style>
+
