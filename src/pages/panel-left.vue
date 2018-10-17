6bed67d616d1c9e4552f276ae455577cb5842449
khactam <khactam95@gmail.com>
2018-10-17 18:27:45 +0300

    modify left panel

--- src/pages/panel-left.vue
+++ src/pages/panel-left.vue
@@ -1,12 +1,32 @@
 <template>
   <f7-page>
-    <f7-navbar title="BIO"></f7-navbar>
+    <f7-navbar title="About Me"></f7-navbar>
+    <f7-card style="background-color:transparent;box-shadow:none;">
+        <img src="../../static/img/peachpit.jpg" width="100%" style="border-radius:50%" />
+    </f7-card>
+    <f7-card>
+      <f7-card-header>
+        BIO
+      </f7-card-header>
+      <f7-card-content>
+        <b>Fullname</b>: Khac Tam Nguyen
+        <br>
+        <b>Funnyname</b>: Tommi
+        <br>
+        <b>D.O.B</b>: 23.3.1995
+        <br>
+        <b>Nationality</b>: Vietnam
+        <br>
+        <b>Zodiac</b>: Aries
+      </f7-card-content>
+    </f7-card>
     <f7-card
   content="Hello, few words about me. I'm a developer who love doing right thing but still drink when I code so thing is not so right. Love being somewhere that is strange and new (can be a new house of a lady) or just being somewhere peaceful (like in random lady's bed)."></f7-card>
     <f7-list media-list inset>
       <f7-list-item
-        link="https://www.youtube.com/watch?v=PRXtbLqIx04"
         title="My anthem"
+        link="https://www.youtube.com/watch?v=PRXtbLqIx04"
+        external
         subtitle="Peach Pit">
         <img slot="media" src="../../static/img/peachpit.jpg" width="50" />
       </f7-list-item>
