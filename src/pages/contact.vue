6568ff093b8b316c6af4040e6ca21f384849abd8
khactam <khactam95@gmail.com>
2018-10-30 16:33:19 +0200

    send grid function added

--- src/pages/contact.vue
+++ src/pages/contact.vue
@@ -23,7 +23,7 @@
         <f7-input :value="dataInfo.description" @input="dataInfo.description = $event.target.value" type="textarea" resizable placeholder="Your sharing?"></f7-input>
       </f7-list-item>
     </f7-list>
-    <f7-button style="margin:20px;" big raised outline v-on:click="send">Send</f7-button>
+    <f7-button style="margin:20px;" big raised outline v-on:click="sendEmail">Send</f7-button>
   </f7-page>
 </template>
 <script>
@@ -35,22 +35,21 @@
           number:'',
           email:'',
           description:''
-        }
+        },
+        apikey: "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"
       }
     },
     methods: {
       send (e) {
-        if (this.dataInfo.name && this.dataInfo.number) {
-          return true;
+        if (this.dataInfo.name && this.dataInfo.number && this.dataInfo.email && this.validEmail(this.dataInfo.email)) {
+          console.log('OK');
+          sendEmail()
         }
-
-        this.errors = [];
-
         if (!this.dataInfo.name) {
-          this.errors.push('Name required.');
+          console.log('Name required.');
         }
         if (!this.dataInfo.number) {
-          this.errors.push('Number required.');
+          console.log('Number required.');
         }
         if (!this.dataInfo.email) {
           console.log('Email required.');
@@ -61,6 +60,45 @@
       validEmail: function (email) {
         var re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
         return re.test(email);
+      },
+      sendEmail: function() {
+        console.log("sent");
+        let data = {
+            "personalizations": [
+              {
+                "to": [
+                  {
+                    "email": "khactam95@gmail.com"
+                  }
+                ],
+                "subject": "Hello, World!"
+              }
+            ],
+            "from": {
+              "email": this.dataInfo.email,
+              "name": "test"
+            },
+            "reply_to": {
+              "email": "khactam95@gmail.com",
+              "name": "Tam Nguyen"
+            },
+            "content": [
+              {
+                "type": "text/plain",
+                "value": "Hello, World!"
+              }
+            ]
+          }
+        axios.post('https://api.sendgrid.com/v3/mail/send' , data, {
+          headers: {
+            'Authorization' : 'Bearer ' + this.apikey,
+            'Content-Type': 'application/json',
+            "Access-Control-Allow-Origin": "*"
+          },
+        })
+        .then((response) => {
+          console.log(response.data);
+        })
       }
     }
   }
