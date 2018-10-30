7f71ee2ba15ac4b13c48b24dfaba313d10ea3d53
khactam <khactam95@gmail.com>
2018-10-30 16:33:35 +0200

    changing text color

--- src/pages/skills.vue
+++ src/pages/skills.vue
@@ -18,7 +18,7 @@
             :value-text="`80%`"
             :value-font-size="10"
             :border-width="7"
-            value-text-color="#ffffff"
+            value-text-color="#002766"
             id="gauge-center"
           ></f7-gauge>
         </f7-col>
@@ -32,41 +32,149 @@
             :value-text="`70%`"
             :value-font-size="10"
             :border-width="7"
-            value-text-color="#ffffff"
+            value-text-color="#002766"
             id="gauge-center"
           ></f7-gauge>
         </f7-col>
       </f7-row>
     </f7-card>
     <f7-card>
-      <f7-card-content>
-        <f7-block-header>IONIC</f7-block-header>
-        <p><f7-progressbar class="color-red" :progress="80" id="demo-inline-progressbar"></f7-progressbar></p>
-      </f7-card-content>
-      <f7-card-content>
-        <f7-block-header>CORDOVA</f7-block-header>
-        <p><f7-progressbar class="color-red" :progress="80" id="demo-inline-progressbar"></f7-progressbar></p>
-      </f7-card-content>
-      <f7-card-content>
-        <f7-block-header>NODEJS</f7-block-header>
-        <p><f7-progressbar class="color-red" :progress="75" id="demo-inline-progressbar"></f7-progressbar></p>
-      </f7-card-content>
-      <f7-card-content>
-        <f7-block-header>PHP</f7-block-header>
-        <p><f7-progressbar class="color-red" :progress="55" id="demo-inline-progressbar"></f7-progressbar></p>
-      </f7-card-content>
-      <f7-card-content>
-        <f7-block-header>FRAMEWORK 7</f7-block-header>
-        <p><f7-progressbar class="color-red" :progress="55" id="demo-inline-progressbar"></f7-progressbar></p>
-      </f7-card-content>
-      <f7-card-content>
-        <f7-block-header>SWIFT</f7-block-header>
-        <p><f7-progressbar class="color-red" :progress="50" id="demo-inline-progressbar"></f7-progressbar></p>
-      </f7-card-content>
-      <f7-card-content>
-        <f7-block-header>REACT</f7-block-header>
-        <p><f7-progressbar class="color-red" :progress="40" id="demo-inline-progressbar"></f7-progressbar></p>
-      </f7-card-content>
+      <f7-row>
+        <f7-col class="center-grid">
+          <f7-block>IONIC</f7-block>
+          <f7-gauge
+            type="circle"
+            :value="0.8"
+            :size="50"
+            border-color="#0b0b70"
+            :value-text="`90%`"
+            :value-font-size="10"
+            :border-width="7"
+            value-text-color="#002766"
+            id="gauge-center"
+          ></f7-gauge>
+        </f7-col>
+        <f7-col class="center-grid">
+          <f7-block>CORDOVA</f7-block>
+          <f7-gauge
+            type="circle"
+            :value="0.7"
+            :size="50"
+            border-color="#0b0b70"
+            :value-text="`75%`"
+            :value-font-size="10"
+            :border-width="7"
+            value-text-color="#002766"
+            id="gauge-center"
+          ></f7-gauge>
+        </f7-col>
+      </f7-row>
+    </f7-card>
+    <f7-card>
+      <f7-row>
+        <f7-col class="center-grid">
+          <f7-block>ANGULAR</f7-block>
+          <f7-gauge
+            type="circle"
+            :value="0.8"
+            :size="50"
+            border-color="#0b0b70"
+            :value-text="`60%`"
+            :value-font-size="10"
+            :border-width="7"
+            value-text-color="#002766"
+            id="gauge-center"
+          ></f7-gauge>
+        </f7-col>
+        <f7-col class="center-grid">
+          <f7-block>NODEJS</f7-block>
+          <f7-gauge
+            type="circle"
+            :value="0.7"
+            :size="50"
+            border-color="#0b0b70"
+            :value-text="`65%`"
+            :value-font-size="10"
+            :border-width="7"
+            value-text-color="#002766"
+            id="gauge-center"
+          ></f7-gauge>
+        </f7-col>
+        <f7-col class="center-grid">
+          <f7-block>REACT</f7-block>
+          <f7-gauge
+            type="circle"
+            :value="0.7"
+            :size="50"
+            border-color="#0b0b70"
+            :value-text="`50%`"
+            :value-font-size="10"
+            :border-width="7"
+            value-text-color="#002766"
+            id="gauge-center"
+          ></f7-gauge>
+        </f7-col>
+      </f7-row>
+    </f7-card>
+    <f7-card>
+      <f7-row>
+        <f7-col class="center-grid">
+          <f7-block>SWIFT</f7-block>
+          <f7-gauge
+            type="circle"
+            :value="0.8"
+            :size="50"
+            border-color="#0b0b70"
+            :value-text="`40%`"
+            :value-font-size="10"
+            :border-width="7"
+            value-text-color="#002766"
+            id="gauge-center"
+          ></f7-gauge>
+        </f7-col>
+        <f7-col class="center-grid">
+          <f7-block>JAVA</f7-block>
+          <f7-gauge
+            type="circle"
+            :value="0.7"
+            :size="50"
+            border-color="#0b0b70"
+            :value-text="`40%`"
+            :value-font-size="10"
+            :border-width="7"
+            value-text-color="#002766"
+            id="gauge-center"
+          ></f7-gauge>
+        </f7-col>
+        <f7-col class="center-grid">
+          <f7-block>C#</f7-block>
+          <f7-gauge
+            type="circle"
+            :value="0.7"
+            :size="50"
+            border-color="#0b0b70"
+            :value-text="`50%`"
+            :value-font-size="10"
+            :border-width="7"
+            value-text-color="#002766"
+            id="gauge-center"
+          ></f7-gauge>
+        </f7-col>
+        <f7-col class="center-grid">
+          <f7-block>PHP</f7-block>
+          <f7-gauge
+            type="circle"
+            :value="0.7"
+            :size="50"
+            border-color="#0b0b70"
+            :value-text="`40%`"
+            :value-font-size="10"
+            :border-width="7"
+            value-text-color="#002766"
+            id="gauge-center"
+          ></f7-gauge>
+        </f7-col>
+      </f7-row>
     </f7-card>
   </f7-page>
 </template>
@@ -96,6 +204,7 @@ export default {
 }
 .center-grid{
   text-align:center;
+  padding: 10px;
 }
 </style>
 
