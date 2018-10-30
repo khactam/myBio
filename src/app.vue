f401b4eb392cd4cc253c40625132e762c8fcade8
khactam <khactam95@gmail.com>
2018-10-30 16:32:21 +0200

    remove dark theme

--- src/app.vue
+++ src/app.vue
@@ -16,7 +16,7 @@
     </f7-panel>
 
     <!-- Main View -->
-    <f7-view id="main-view" url="/" main theme-dark></f7-view>
+    <f7-view id="main-view" url="/" main></f7-view>
 
     <!-- Popup -->
     <f7-popup id="popup">
